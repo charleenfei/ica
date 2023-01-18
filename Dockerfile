77d0392495df1d4ab3ddb7ab83b13eed613e023c
stvjin007 <stvjin007@localhost>
2023-01-19 06:47:18 +0700

    fix unitest error

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /home/ubuntu
 
 env PATH /usr/local/go/bin:$PATH
 
-RUN apt-get update && apt-get install -y wget make
+RUN apt-get update && apt-get install -y wget make jq
 RUN wget https://go.dev/dl/go1.18.9.linux-amd64.tar.gz
 RUN tar -C /usr/local -xzvf go1.18.9.linux-amd64.tar.gz
 
--- network/unitest.sh
+++ network/unitest.sh
@@ -1,4 +1,12 @@
+set -e
+
+echo "************ INTERCHAIN ACCOUNT DEMO UNITEST ************"
+
 export $(cat data/oracle/docker.env | xargs)
 
-echo "setup ICA account from controller chain (ChainId: test-1)"
-echo y | icad tx intertx register --from $WALLET_1 --connection-id connection-0 --chain-id test-1 --home ./data/test-1 --node tcp://chain-test-1:16657 --keyring-backend test --timeout-height 1000
+CONTROLLERCHAIN_ID=chain-test-1
+HOSTCHAIN_ID=chain-test-2
+
+echo "[EXECUTING] setup ICA account from controller chain (ChainId: test-1)"
+txhash=$(echo y | icad tx intertx register --from $WALLET_1 --connection-id connection-0 --chain-id test-1 --home ./data/test-1 --node tcp://${CONTROLLERCHAIN_ID}:16657 --keyring-backend test --timeout-height 1000 --broadcast-mode block --output json | jq -r '.txhash' | sed 's/null//g' | xargs)
+echo "[INFO] txhash: ${txhash}"
