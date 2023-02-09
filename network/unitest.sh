5fac1a80b4a4b91c86224cd8b5423a712a648450
stvjin007 <stvjin007@localhost>
2023-02-09 15:29:40 +0700

    add unitest

--- network/unitest.sh
+++ network/unitest.sh
@@ -16,7 +16,7 @@ txhash=$(icad tx intertx register --from $WALLET_1 --connection-id ${CONNECTION_
 echo "[INFO] txhash: ${txhash}"
 
 echo "[INFO] Query the address of the interchain account..."
-sleep 5
+sleep 10
 ICA_ADDR=$(icad query intertx interchainaccounts ${CONNECTION_ID} $WALLET_1 --home ./data/${CMP_CONTROLLER_CHAIN_ID} --node ${CMP_CONTROLLER_CHAIN_URL} --output json | jq -r '.interchain_account_address')
 echo "[INFO] interchain_account_address: ${ICA_ADDR}"
 
@@ -34,7 +34,7 @@ txhash=$(icad tx controller submit-tx \
     \"metadata\":\"test_meta_data\"
 }" ${CONNECTION_ID} --from $WALLET_1 --chain-id ${CMP_CONTROLLER_CHAIN_ID} --home ./data/${CMP_CONTROLLER_CHAIN_ID} --node ${CMP_CONTROLLER_CHAIN_URL} --keyring-backend test -y --broadcast-mode block --output json | jq -r '.txhash')
 echo "[INFO] txhash: ${txhash}"
-sleep 5
+sleep 10
 
 expected_whois_size=$(icad q nameservice list-whois --chain-id ${CMP_HOST_CHAIN_ID} --home ./data/${CMP_HOST_CHAIN_ID} --node ${CMP_HOST_CHAIN_URL} --output json | jq -r '.whois | length')
 
@@ -61,7 +61,7 @@ txhash=$(icad tx controller submit-tx \
     \"metadata\":\"test_meta_data\"
 }" ${CONNECTION_ID} --from $WALLET_1 --chain-id ${CMP_CONTROLLER_CHAIN_ID} --home ./data/${CMP_CONTROLLER_CHAIN_ID} --node ${CMP_CONTROLLER_CHAIN_URL} --keyring-backend test -y --broadcast-mode block --output json | jq -r '.txhash')
 echo "[INFO] txhash: ${txhash}"
-sleep 5
+sleep 10
 
 expected_whois_size=$(icad q nameservice list-whois --chain-id ${CMP_HOST_CHAIN_ID} --home ./data/${CMP_HOST_CHAIN_ID} --node ${CMP_HOST_CHAIN_URL} --output json | jq -r '.whois | length')
 
@@ -72,5 +72,65 @@ if [ ${expected_whois_size} -eq 1 ]; then
 else
     echo
     echo "[ERROR!!!] DNS Item List is STILL empty..."
+    exit 1
+fi
+
+echo
+echo "****** WORKFLOW A: Banned domain cannot be bought ******"
+echo
+
+echo "[INFO] In the default configuration, .country-x domain is banned..."
+echo "[EXECUTING] Try buying a .country-x domain..."
+txhash=$(icad tx controller submit-tx \
+"{
+    \"@type\":\"/cosmos.interchainaccounts.nameservice.MsgCmpBuy\",
+    \"creator\": \"$ICA_ADDR\",
+    \"name\":\"testdomain.country-x\",
+    \"bid\":\"200\",
+    \"metadata\":\"test_meta_data\"
+}" ${CONNECTION_ID} --from $WALLET_1 --chain-id ${CMP_CONTROLLER_CHAIN_ID} --home ./data/${CMP_CONTROLLER_CHAIN_ID} --node ${CMP_CONTROLLER_CHAIN_URL} --keyring-backend test -y --broadcast-mode block --output json | jq -r '.txhash')
+
+echo "[INFO] txhash: ${txhash}"
+sleep 10
+
+expected_whois_size=$(icad q nameservice list-whois --chain-id ${CMP_HOST_CHAIN_ID} --home ./data/${CMP_HOST_CHAIN_ID} --node ${CMP_HOST_CHAIN_URL} --output json | jq -r '.whois | length')
+
+if [ ${expected_whois_size} -eq 1 ]; then
+    echo
+    echo "[SUCCESS!!!] DNS Item List is expected to have one record..."
+    echo
+else
+    echo
+    echo "[ERROR!!!] DNS Item List is NOT having one record..."
+    icad q nameservice list-whois --chain-id ${CMP_HOST_CHAIN_ID} --home ./data/${CMP_HOST_CHAIN_ID} --node ${CMP_HOST_CHAIN_URL} --output json
+    
+    exit 1
+fi
+
+echo "[INFO] To unban .country-x domain"
+sed -i '2s/"country-x"//' /oracle/cmp_config.json
+
+echo "[EXECUTING] Re-try buying a .country-x domain..."
+txhash=$(icad tx controller submit-tx \
+"{
+    \"@type\":\"/cosmos.interchainaccounts.nameservice.MsgCmpBuy\",
+    \"creator\": \"$ICA_ADDR\",
+    \"name\":\"testdomain.country-x\",
+    \"bid\":\"200\",
+    \"metadata\":\"test_meta_data\"
+}" ${CONNECTION_ID} --from $WALLET_1 --chain-id ${CMP_CONTROLLER_CHAIN_ID} --home ./data/${CMP_CONTROLLER_CHAIN_ID} --node ${CMP_CONTROLLER_CHAIN_URL} --keyring-backend test -y --broadcast-mode block --output json | jq -r '.txhash')
+
+echo "[INFO] txhash: ${txhash}"
+sleep 10
+
+expected_whois_size=$(icad q nameservice list-whois --chain-id ${CMP_HOST_CHAIN_ID} --home ./data/${CMP_HOST_CHAIN_ID} --node ${CMP_HOST_CHAIN_URL} --output json | jq -r '.whois | length')
+
+if [ ${expected_whois_size} -eq 2 ]; then
+    echo
+    echo "[SUCCESS!!!] DNS Item List is expected to have two record..."
+    echo
+else
+    echo
+    echo "[ERROR!!!] DNS Item List is NOT having two record..."
     exit 1
 fi
