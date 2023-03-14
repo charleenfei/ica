10c44ff519e97f788e99693332d318bacc2bd191
TN Khanh <tnkhanh4@gmail.com>
2023-03-14 11:25:57 +0800

    Update

--- pydemo/main/account.js
+++ pydemo/main/account.js
@@ -3,31 +3,39 @@ let addressElement = document.getElementById('address');
 let nameElement = document.getElementById('name');
 let chainElement = document.getElementById('chain');
 let rCom = document.getElementById('r-com');
-let rAddr = document.getElementById('r-addr');
-let rChain = document.getElementById('r-chain');
 let rItem = document.getElementById('r-item');
 let rPrice = document.getElementById('r-price');
 
-function addChild(element, cont, cClass) {
-  let node = document.createElement('div');
-  if (cClass !== '') node.classList.add(cClass);
-  node.innerHTML = cont;
-  element.appendChild(node);
-}
+let icaTxt = document.getElementById('ica-txt');
+let balTxt = document.getElementById('bal-txt');
+let spinner = document.getElementById('spinner');
 
 function register() {
+  addressElement.readOnly = true;
+  nameElement.readOnly = true;
+  chainElement.readOnly = true;
+
   fetch('/register/' + addressElement.value +'/' + nameElement.value + '/' + chainElement.value)
     .then((response) => response.text())
     .then((ica) => {
-      infoElement.innerHTML = '<tr><td>Name: ' + nameElement.value + "</td></tr>"  
-      + '<tr><td>Address: ' + addressElement.value + "</td></tr>"
-      + '<tr><td>Chain: ' + chainElement.value + "</td></tr>"
-      + '<tr><td>ICA Address: ' + ica + "</td></tr>"
+      icaTxt.innerHTML = ica;
+      loadBalance();
     });
 }
 
+function loadBalance() {
+  spinner.classList.remove('visually-hidden');
+  ica = icaTxt.innerHTML;
+  fetch('/balance/' + ica)
+    .then((response) => response.text())
+    .then((balance) => {
+      balTxt.innerHTML = balance;
+    });
+  spinner.classList.add('visually-hidden');
+}
+
 function run() {
-  fetch('/run/' + rCom.value + '/' + rAddr.value + '/' + rChain.value + '/' + rItem.value + '/' + rPrice.value);
+  fetch('/run/' + rCom.value + '/' + addressElement.value + '/' + chainElement.value + '/' + rItem.value + '/' + rPrice.value);
 }
 
 //setInterval(reload, 2000);
